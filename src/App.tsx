a3cd9a0a61818e619965166219d161939ce2f92c
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-18 21:33:39 +0300

    create file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+
+const App = () => {
+
+
+    return (
+        <div className={'App'}>
+           <input value='Max' placeholder='Enter name'/>
+        </div>
+    )
+}
+
+export default App
