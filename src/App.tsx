df18937a9bd9016c70fee9f843c0156670f76d06
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-18 21:50:35 +0300

    1. useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import './App.css'
 
+type dataType = {
+    name:string
+}
 const App = () => {
-
+    const [name, setName] = useState('')
+    const [data, setData] = useState<dataType>({} as dataType)
 
     return (
         <div className={'App'}>
-           <input value='Max' placeholder='Enter name'/>
+            <input value={name} placeholder='Enter name' onChange={e => setName(e.currentTarget.value)}/>
+            <input value={data.name} placeholder='Enter name' onChange={e => setData({...data,name:e.currentTarget.value})}/>
+            <div><b>value:
+                {name}</b></div>
+            <div><b>value:
+                {data.name}</b></div>
         </div>
+
     )
 }
 
